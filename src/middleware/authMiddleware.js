85096ae3db6c7eb26358be7e989555d8bdd7787b
Gourav-mlv <malviyagourav07@gmail.com>
2025-03-20 00:28:13 +0530

    moved to ObjectId based approach along with that added a mapping function

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+exports.authenticate = (req, res, next) => {
+  const token = req.header("Authorization");
+  
+  if (!token) return res.status(401).json({ message: "Access Denied. No token provided." });
+
+  try {
+    const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
+    console.log(decoded);
+    req.user = decoded.username;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Invalid token" });
+  }
+};
---
+++ src/routes/categoryRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const categoryService = require("../controllers/categoryService");
+const { authenticate } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.get('/categories/:id', authenticate, categoryService.getCategories);
+router.post('/categories' , authenticate,  categoryService.createCategory);
+router.put('/categories' ,  authenticate, categoryService.updateCategory);
+router.delete('/categories' ,  authenticate, categoryService.deleteCategory);
+router.get('/map/categories' ,  authenticate, categoryService.getMappings);
+
+
+module.exports = router;
